ac294583ef98032baea9ad780894001d0b1131fa
dakshilgorasiya <dakshilgorasiya2005@gmail.com>
2024-02-24 17:53:19 +0530

    10_todo_context_local updated

--- 10_todo_context_local/src/components/TodoForm.jsx
+++ 10_todo_context_local/src/components/TodoForm.jsx
@@ -1,12 +1,26 @@
-import React from "react";
+import React, { useState } from "react";
+import { useTodo } from "../context";
 
 function TodoForm() {
+    const [todo, setTodo] = useState("");
+
+    const { addTodo } = useTodo();
+
+    const add = (e) => {
+        e.preventDefault();
+
+        if (!todo) return;
+        addTodo({ todo, completed: false });
+        setTodo("");
+    };
     return (
-        <form className="flex">
+        <form className="flex" onSubmit={add}>
             <input
                 type="text"
                 placeholder="Write Todo..."
                 className="w-full border border-black/10 rounded-l-lg px-3 outline-none duration-150 bg-white/20 py-1.5"
+                onChange={(e) => setTodo(e.target.value)}
+                value={todo}
             />
             <button
                 type="submit"
--- 10_todo_context_local/src/components/TodoItem.jsx
+++ 10_todo_context_local/src/components/TodoItem.jsx
@@ -1,6 +1,20 @@
-import React from "react";
+import React, { useState } from "react";
+import { useTodo } from "../context";
 
 function TodoItem({ todo }) {
+    const [isTodoEditable, setIsTodoEditable] = useState(false);
+    const [todoMsg, setTodoMsg] = useState(todo.todo);
+    const { updateTodo, deleteTodo, toggleCompleted } = useTodo();
+
+    const toggle = () => {
+        toggleCompleted(todo.id);
+    };
+
+    const editTodo = () => {
+        updateTodo(todo.id, { ...todo, todo: todoMsg });
+        setIsTodoEditable(false);
+    };
+
     return (
         <div
             className={`flex border border-black/10 rounded-lg px-3 py-1.5 gap-x-3 shadow-sm shadow-white/50 duration-300  text-black ${
@@ -11,7 +25,7 @@ function TodoItem({ todo }) {
                 type="checkbox"
                 className="cursor-pointer"
                 checked={todo.completed}
-                onChange={toggleCompleted}
+                onChange={toggle}
             />
             <input
                 type="text"
